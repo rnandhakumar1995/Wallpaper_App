cf763c99ef9c78f8d18124bdcb7776d3e25c9c2b
nandha_the_ken <nandha@the-ken.com>
2021-12-28 09:59:56 +0530

    Added loading indicator

--- app/src/main/java/io/nandha/wallpaperapp/ui/detailactivity/IndividualItemAdapter.kt
+++ app/src/main/java/io/nandha/wallpaperapp/ui/detailactivity/IndividualItemAdapter.kt
@@ -1,6 +1,5 @@
 package io.nandha.wallpaperapp.ui.detailactivity
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import io.nandha.wallpaperapp.R
+import io.nandha.wallpaperapp.core.utils.showLoading
 import io.nandha.wallpaperapp.data.model.Image
 
 class IndividualItemAdapter(private val images: List<Image>) :
@@ -22,7 +22,8 @@ class IndividualItemAdapter(private val images: List<Image>) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.individual_image_item, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.individual_image_item, parent, false)
         return ViewHolder(view)
     }
 
@@ -31,6 +32,7 @@ class IndividualItemAdapter(private val images: List<Image>) :
         Glide
             .with(holder.itemView.context)
             .load(image.hdurl)
+            .showLoading(holder.itemView.context)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(holder.image)
         holder.title.text = image.title
--- app/src/main/java/io/nandha/wallpaperapp/ui/mainactivity/ImageItemAdapter.kt
+++ app/src/main/java/io/nandha/wallpaperapp/ui/mainactivity/ImageItemAdapter.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import io.nandha.wallpaperapp.R
+import io.nandha.wallpaperapp.core.utils.showLoading
 import io.nandha.wallpaperapp.data.model.Image
 import io.nandha.wallpaperapp.ui.detailactivity.DetailActivity
 
@@ -29,15 +30,15 @@ class ImageItemAdapter(private val images: List<Image>) :
         val image = images[holder.adapterPosition]
         Glide
             .with(holder.itemView.context)
-            .load(image.url)
+            .load(image.url).showLoading(holder.itemView.context)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(holder.image)
         holder.title.text = image.title
         holder.itemView.setOnClickListener {
-            it.context?.also {
-                val intent = Intent(it, DetailActivity::class.java)
+            it.context?.also { context ->
+                val intent = Intent(context, DetailActivity::class.java)
                 intent.putExtra("position", holder.adapterPosition)
-                it.startActivity(intent)
+                context.startActivity(intent)
             }
         }
 
