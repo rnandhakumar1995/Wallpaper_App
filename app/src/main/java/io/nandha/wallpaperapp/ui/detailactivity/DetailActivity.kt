7c0f99989d9224ad3f1a7fdb59edbe8933c58522
nandha_the_ken <nandha@the-ken.com>
2021-12-28 10:04:25 +0530

    Added back button in DetailActivity.kt

--- app/src/main/java/io/nandha/wallpaperapp/ui/detailactivity/DetailActivity.kt
+++ app/src/main/java/io/nandha/wallpaperapp/ui/detailactivity/DetailActivity.kt
@@ -3,8 +3,6 @@ package io.nandha.wallpaperapp.ui.detailactivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearSmoothScroller
-import androidx.recyclerview.widget.RecyclerView.SmoothScroller
 import io.nandha.wallpaperapp.databinding.ActivityDetailBinding
 
 
@@ -15,12 +13,19 @@ class DetailActivity : AppCompatActivity() {
             layoutInflater
         )
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
     private val viewModel: DetailActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val position = intent.extras?.getInt("position") ?: 1
         val adapter = IndividualItemAdapter(viewModel.data)
