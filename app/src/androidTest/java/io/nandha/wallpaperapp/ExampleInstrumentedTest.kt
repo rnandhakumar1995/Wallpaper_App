0c94f67a17bc4bbeb269c892abe1355179b4bf04
nandha_the_ken <nandha@the-ken.com>
2021-12-29 11:05:43 +0530

    Added testcases to test next button Added testcases to test title from the bottomsheet

--- app/src/androidTest/java/io/nandha/wallpaperapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/nandha/wallpaperapp/ExampleInstrumentedTest.kt
@@ -1,6 +1,6 @@
 package io.nandha.wallpaperapp
 
-import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
@@ -11,15 +11,48 @@ import io.nandha.wallpaperapp.ui.mainactivity.ImageItemAdapter
 import io.nandha.wallpaperapp.ui.mainactivity.MainActivity
 import org.hamcrest.Matchers
 import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.containsString
+import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
 
 
 @RunWith(AndroidJUnit4::class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class ExampleInstrumentedTest {
     @get: Rule
     val mainActivity = ActivityScenarioRule(MainActivity::class.java)
+
+
+    @Test
+    fun aTestClickOnItem() {
+        goToPosition(3)
+//        checkCurrentPositionBasedOnTag("A Distorted Sunrise Eclipse")
+    }
+
+    @Test
+    fun bTestPreviousButton() {
+        goToPosition(2)
+        onIdle()
+        onView(withId(R.id.previous)).perform(click()).perform(click()).perform(
+            click()
+        ).perform(click()).perform(click())
+        checkCurrentPositionBasedOnTag("M33: The Triangulum Galaxy")
+    }
+
+    @Test
+    fun cTestNextButton() {
+        goToPosition(10)
+        onIdle()
+        onView(withContentDescription(R.string.next)).perform(click()).perform(click())
+            .perform(click()).perform(click()).perform(click()).perform(click()).perform(click())
+            .perform(click()).perform(click()).perform(click()).perform(click()).perform(click())
+            .perform(click()).perform(click()).perform(click()).perform(click())
+            .check(matches(isDisplayed()))
+        checkCurrentPositionBasedOnTag("Starburst Galaxy M94 from Hubble")
+    }
 
     private fun checkCurrentPositionBasedOnTag(tagText: String) {
         onView(
@@ -29,25 +62,16 @@ class ExampleInstrumentedTest {
             )
         ).check(
             matches(isDisplayed())
-        )
+        ).perform(click())
+        onView(withId(R.id.title)).check(matches(withText(containsString(tagText))))
     }
 
-    @Test
-    fun testClickOnItem() {
+    private fun goToPosition(position: Int) {
         onView(withId(R.id.imageGrid)).perform(
             actionOnItemAtPosition<ImageItemAdapter.ViewHolder>(
-                3,
+                position,
                 click()
             )
         )
-        checkCurrentPositionBasedOnTag("A Distorted Sunrise Eclipse")
-        testPreviousButton()
-    }
-
-    private fun testPreviousButton() {
-        onView(withId(R.id.previous)).perform(click()).perform(click()).perform(
-            click()
-        ).perform(click()).perform(click())
-        checkCurrentPositionBasedOnTag("M33: The Triangulum Galaxy")
     }
 }
