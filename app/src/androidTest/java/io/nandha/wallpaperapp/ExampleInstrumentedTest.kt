28df8e0bae705690a0bd21090e5f1782131d2440
nandha_the_ken <nandha@the-ken.com>
2021-12-29 07:13:27 +0530

    Added basic UI flow test

--- app/src/androidTest/java/io/nandha/wallpaperapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/nandha/wallpaperapp/ExampleInstrumentedTest.kt
@@ -1,24 +1,62 @@
 package io.nandha.wallpaperapp
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.view.View
+import androidx.core.view.get
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.viewpager2.widget.ViewPager2
+import io.nandha.wallpaperapp.ui.mainactivity.ImageItemAdapter
+import io.nandha.wallpaperapp.ui.mainactivity.MainActivity
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    fun atPosition(position: Int, itemMatcher: Matcher<View>): BoundedMatcher<View?, ViewPager2> {
+        return object : BoundedMatcher<View?, ViewPager2>(ViewPager2::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
+            }
+
+            override fun matchesSafely(view: ViewPager2): Boolean {
+                val viewHolder =
+                    (view[0] as RecyclerView).findViewHolderForAdapterPosition(position)
+                        ?: // has no item on such position
+                        return false
+                return itemMatcher.matches(viewHolder.itemView.findViewById(R.id.expand))
+            }
+        }
+    }
+
+    @get: Rule
+    val mainActivity = ActivityScenarioRule(MainActivity::class.java)
+
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.nandha.wallpaperapp", appContext.packageName)
+    fun testClickOnItem() {
+        onView(withId(R.id.imageGrid)).perform(
+            actionOnItemAtPosition<ImageItemAdapter.ViewHolder>(
+                3,
+                click()
+            )
+        )
+        onView(allOf(withId(R.id.expand), withTagValue(Matchers.`is`("A Distorted Sunrise Eclipse")))).check(
+            matches(isDisplayed())
+        )
     }
 }
